328bedbdc5ad5e8186104322ebc5bd101c2f9ce4
Xiao Zhang <xiaozha@0587450602.wireless.umich.net>
2025-10-07 15:33:23 -0400

    update init

--- semiconductor_db/__init__.py
+++ semiconductor_db/__init__.py
@@ -1,3 +1,4 @@
 from .convergence import ConvergenceDB, get_kpt_convergence, get_encut_convergence
+from .e_v_db import e_v_db
 
-__all__ = ["ConvergenceDB", "get_kpt_convergence", "get_encut_convergence"]
+__all__ = ["ConvergenceDB", "get_kpt_convergence", "get_encut_convergence","e_v_db"]
